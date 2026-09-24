0ce052cadd49b1c347095e916ccbac02ee5f9905
hymozarog-jp <hymozarog@gmail.com>
2026-09-24 14:08:18 +0200

    Use correct Garama and Madundung salt and honey image

--- product-images.js
+++ product-images.js
@@ -4,7 +4,7 @@ function productImage(name=''){
   if(n.includes('shahid')||n.includes('شاهد'))return 'assets/shahid.svg';
   if(n.includes('spotify')||n.includes('سبوتيفاي'))return 'assets/spotify.svg';
   if(n.includes('roblox')||n.includes('robux')||n.includes('روبلوكس'))return 'assets/robux.svg';
-  if(n.includes('قرما اند مدوندونق')||n.includes('garama and madundung')||n.includes('garama')&&n.includes('madundung'))return 'https://yumcut.com/brainrot-data/garama-and-madundung/original/prepared.webp';
+  if(n.includes('قرما اند مدوندونق')||n.includes('garama and madundung')||n.includes('garama')&&n.includes('madundung'))return 'https://a.allegroimg.com/original/11ae7b/409f1a834918b309960ad224da26/Garama-and-Madundung-Steal-A-Brainrot-Roblox';
   if(n.includes('دراقون')||n.includes('dragon cannelloni')||n.includes('cannelloni'))return 'https://production-gameflipusercontent.fingershock.com/us-east-1%3A71f60d98-b8ed-4dec-b31b-28001bfdaf0a/f5b51031-e17c-4e7a-ab2a-81775ed0b511/abea5f1c-99c2-4bbc-9c95-803d2708ec93/640x640.webp';
   return '';
 }
